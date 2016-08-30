d783a413796687742616778990514e18110e9420
Denis Silakov <dsilakov@virtuozzo.com>
2016-08-30 11:58:10 +0300

    Added option to install bootloader

--- vzupgrade.py
+++ vzupgrade.py
@@ -50,13 +50,19 @@ def install():
         print "It looks like preupgrade check was not performed, launching..."
         cmdline.blocker = False
         check()
+
+    if cmdline.boot:
+        with open("/root/preupgrade/postupgrade.d/pkgdowngrades", "a") as cfg_file:
+            cfg_file.write("grub2-mkconfig -O /boot/grub2/grub.cfg 2>&1 | tee -a /var/log/vzupgrade.log")
+            cfg_file.write("grub2-install " + cmdline.boot + " 2>&1 | tee -a /var/log/vzupgrade.log")
+
     if cmdline.device:
         subprocess.call(['redhat-upgrade-tool', '--device', cmdline.device, '--cleanup-post'])
     elif cmdline.network:
         subprocess.call(['redhat-upgrade-tool', '--network', '7.0', '--instrepo', cmdline.network, '--cleanup-post'])
 
 def list_prereq():
-    print "=== Virtuozzo-specifi upgrade prerequisites: ==="
+    print "=== Virtuozzo-specific upgrade prerequisites: ==="
     print "* No VMs exist on the host"
     print "* There are no containers that use VZFS"
     print "* There are no templates for OSes not supported by Vz7"
@@ -76,6 +82,7 @@ def parse_command_line():
     sp.set_defaults(func=list_prereq)
 
     sp = subparsers.add_parser('install', help='Perform upgrade')
+    sp.add_argument('--boot', action='store', help='install bootloader to a specified device')
     src_group = sp.add_mutually_exclusive_group(required=True)
     src_group.add_argument('--device', action='store', help='mounted device to be used (please provide link to folder where Vz7 iso image is mounted)')
     src_group.add_argument('--network', action='store', help='Vz7 network repository to be used')
